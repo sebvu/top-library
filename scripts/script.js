cc35344ea637b5575c93ce6f4db237a057cd55c1
Jester <jester.sebastiansantos@gmail.com>
2026-06-15 20:34:24 -0500

    Create Library and Book objects

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+function Book(author, title, pages) {
+  if (!new.target) {
+    throw Error("Book object not initialized with new keyword");
+  }
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  // metadata
+  this.id = crypto.randomUUID();
+}
+
+function Library() {
+  if (!new.target) {
+    throw Error("Library object not initialized with new keyword");
+  }
+  this.books = []; // stores Book objects
+}
+
+// add new book object to books array
+Library.prototype.addBook = function (author, title, pages) {
+  this.books.push(new Book(author, title, pages));
+};
+
+// search book by id and delete from array if found
+Library.prototype.deleteBook = function (id) {
+  if (typeof id !== "string")
+    throw Error(`deleteBook ID type is not string: ${id}`);
+
+  let index = null;
+  this.books.find((book, i) => {
+    if (book.id === id) {
+      index = i;
+      return true;
+    }
+    return false;
+  });
+
+  if (index !== null) {
+    console.log(`Deleting ${this.books[index].title}`);
+    this.books = this.books.slice(0, index).concat(this.books.slice(index + 1));
+  } else {
+    throw Error(`Could not delete book with ID ${id}`);
+  }
+};
+
+function main() {
+  let library = new Library();
+}
+
+main();

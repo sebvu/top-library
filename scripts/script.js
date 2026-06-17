5dddebde62caa908cda78bd7cf0bbc00f5268e0c
Jester <jester.sebastiansantos@gmail.com>
2026-06-17 02:54:26 -0500

    Implement test style for one book

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ function Book(author, title, pages, cover) {
   this.author = author;
   this.title = title;
   this.pages = pages;
+  this.hasRead = false;
   this.cover = cover;
   // metadata
   this.id = crypto.randomUUID();

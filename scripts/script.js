0d63415a03533abf9684fd180d308607d818dc57
Jester <jester.sebastiansantos@gmail.com>
2026-06-23 21:28:49 -0500

    Finish add book dialog popup

--- scripts/script.js
+++ scripts/script.js
@@ -86,22 +86,335 @@ function UILibraryHandler(
 
   // add book handler
   this.addButton.addEventListener("click", () => {
-    const dialog = this.getDialog();
+    const dialog = this._getDialog();
     const containerParent = mainCtr.parentElement;
     containerParent.appendChild(dialog);
+
+    this._buildAddBookDialog(dialog);
 
     dialog.showModal();
   });
 }
 
-// display current books in library
-UILibraryHandler.prototype.buildExistingBooks = function () {
-  const setAttrs = (el, attrs) => {
-    for (let key in attrs) {
-      el.setAttribute(key, attrs[key]);
-    }
+// dialog creation helper
+UILibraryHandler.prototype._getDialog = function () {
+  const dialog = document.createElement("dialog");
+
+  dialog.classList.add(...["dialog", "--size-context-md"]);
+
+  // ensure dialog deletes itself after closing
+  dialog.addEventListener("close", () => {
+    dialog.remove();
+  });
+
+  return dialog;
+};
+
+// attribute setter helper
+UILibraryHandler.prototype._setAttrs = function (el, attrs) {
+  for (let key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+};
+
+// build addBookDialog
+UILibraryHandler.prototype._buildAddBookDialog = function (dialog) {
+  // header
+  const dialogHeader = document.createElement("div");
+  dialogHeader.classList.add("dialog__header");
+  const dialogTitle = document.createElement("h1");
+  dialogTitle.classList.add(
+    ...["dialog__title", "_text", "_text--header", "_text--bold"],
+  );
+  dialogTitle.textContent = "Add Book";
+  const dialogExitButton = document.createElement("button");
+  dialogExitButton.classList.add(...["dialog__exit-button"]);
+  const dialogExitSvg = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg",
+  );
+  dialogExitSvg.classList.add("dialog__exit-svg");
+  this._setAttrs(dialogExitSvg, {
+    class: "dialog__exit-svg",
+    viewBox: "0 0 24 24",
+  });
+  const dialogExitSvgPath = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path",
+  );
+  this._setAttrs(dialogExitSvgPath, {
+    d: "M9,7L11,12L9,17H11L12,14.5L13,17H15L13,12L15,7H13L12,9.5L11,7H9M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2Z",
+  });
+
+  dialogHeader.appendChild(dialogTitle);
+
+  dialogHeader.appendChild(dialogExitButton);
+  dialogExitButton.appendChild(dialogExitSvg);
+  dialogExitSvg.appendChild(dialogExitSvgPath);
+
+  // form
+  const dialogForm = document.createElement("form");
+  dialogForm.classList.add(...["dialog__form", "--size-context-sm"]);
+  this._setAttrs(dialogForm, { action: "", method: "" });
+
+  // form top
+  const dialogFormTop = document.createElement("div");
+  dialogFormTop.classList.add("dialog__form-top");
+
+  const dialogFormPlease = document.createElement("div");
+  dialogFormPlease.classList.add(
+    ...["dialog__form-please", "_text", "_text--normal"],
+  );
+  dialogFormPlease.textContent = "Please complete all required (";
+  const dialogFormPleaseSpan = document.createElement("span");
+  dialogFormPleaseSpan.classList.add(
+    ...["dialog__form-required", "_text", "_text--bold"],
+  );
+  dialogFormPleaseSpan.textContent = "*";
+  dialogFormPlease.appendChild(dialogFormPleaseSpan);
+  dialogFormPlease.appendChild(document.createTextNode(") fields."));
+
+  // p1
+  const DialogFormTopP1 = document.createElement("p");
+  DialogFormTopP1.classList.add(...["dialog__form-top-p"]);
+  const DialogFormTopP1Label = document.createElement("label");
+  DialogFormTopP1Label.classList.add(
+    ...[
+      "dialog__form-label",
+      "dialog__form-label--text",
+      "_text",
+      "_text--normal",
+    ],
+  );
+  DialogFormTopP1Label.setAttribute("for", "title");
+  DialogFormTopP1Label.textContent = "Title:";
+  const DialogFormTopP1Input = document.createElement("input");
+  DialogFormTopP1Input.classList.add(
+    ...[
+      "dialog__form-input",
+      "dialog__form-input--text",
+      "_text",
+      "_text--normal",
+      "_text--bold",
+      "--size-context-xs",
+    ],
+  );
+  this._setAttrs(DialogFormTopP1Input, {
+    type: "text",
+    id: "title",
+    name: "book_title",
+    placeholder: "Hail Mary",
+    required: "",
+  });
+  DialogFormTopP1.appendChild(DialogFormTopP1Label);
+  DialogFormTopP1.appendChild(DialogFormTopP1Input);
+  DialogFormTopP1.appendChild(document.createElement("span"));
+
+  // p2
+  const DialogFormTopP2 = document.createElement("p");
+  DialogFormTopP2.classList.add(...["dialog__form-top-p"]);
+  const DialogFormTopP2Label = document.createElement("label");
+  DialogFormTopP2Label.classList.add(
+    ...[
+      "dialog__form-label",
+      "dialog__form-label--text",
+      "_text",
+      "_text--normal",
+    ],
+  );
+  DialogFormTopP2Label.setAttribute("for", "author");
+  DialogFormTopP2Label.textContent = "Author:";
+  const DialogFormTopP2Input = document.createElement("input");
+  DialogFormTopP2Input.classList.add(
+    ...[
+      "dialog__form-input",
+      "dialog__form-input--text",
+      "_text",
+      "_text--normal",
+      "_text--bold",
+      "--size-context-xs",
+    ],
+  );
+  this._setAttrs(DialogFormTopP2Input, {
+    type: "text",
+    id: "author",
+    name: "book_title",
+    placeholder: "Andy Weir",
+    required: "",
+  });
+  DialogFormTopP2.appendChild(DialogFormTopP2Label);
+  DialogFormTopP2.appendChild(DialogFormTopP2Input);
+  DialogFormTopP2.appendChild(document.createElement("span"));
+
+  // divs
+  const DialogFormTopDiv = document.createElement("div");
+  DialogFormTopDiv.classList.add("dialog__form-top-div");
+
+  const DialogFormTopDivP1 = document.createElement("p");
+  DialogFormTopDivP1.classList.add(
+    ...["dialog__form-top-div-p", "dialog__form-top-div-p--left"],
+  );
+  const DialogFormTopDivP1Label = document.createElement("label");
+  DialogFormTopDivP1Label.classList.add(
+    ...[
+      "dialog__form-label",
+      "dialog__form-label--number",
+      "_text",
+      "_text--normal",
+    ],
+  );
+  DialogFormTopDivP1Label.setAttribute("for", "pages");
+  DialogFormTopDivP1Label.textContent = "Pages:";
+  const DialogFormTopDivP1Input = document.createElement("input");
+  DialogFormTopDivP1Input.classList.add(
+    ...[
+      "dialog__form-input",
+      "dialog__form-input--number",
+      "_text",
+      "_text--normal",
+      "_text--bold",
+      "--size-context-xs",
+    ],
+  );
+  this._setAttrs(DialogFormTopDivP1Input, {
+    type: "number",
+    step: "1",
+    id: "pages",
+    name: "book_pages",
+    placeholder: "496",
+    min: "0",
+    max: "10000",
+    required: "",
+  });
+  DialogFormTopDivP1.appendChild(DialogFormTopDivP1Label);
+  DialogFormTopDivP1.appendChild(DialogFormTopDivP1Input);
+  DialogFormTopDivP1.appendChild(document.createElement("span"));
+
+  const DialogFormTopDivP2 = document.createElement("p");
+  DialogFormTopDivP2.classList.add(
+    ...[
+      "dialog__form-top-div-p",
+      "dialog__form-top-div-p--right",
+      "dialog__form-top-div-p--gap",
+    ],
+  );
+  const DialogFormTopDivP2Label = document.createElement("label");
+  DialogFormTopDivP2Label.classList.add(
+    ...[
+      "dialog__form-label",
+      "dialog__form-label--checkbox",
+      "_text",
+      "_text--normal",
+    ],
+  );
+  DialogFormTopDivP2Label.setAttribute("for", "has-read");
+  DialogFormTopDivP2Label.textContent = "Has Read:";
+  const DialogFormTopDivP2Input = document.createElement("input");
+  DialogFormTopDivP2Input.classList.add(
+    ...[
+      "dialog__form-input",
+      "dialog__form-input--checkbox",
+      "_text",
+      "_text--normal",
+      "_text--bold",
+      "--size-context-xs",
+    ],
+  );
+  this._setAttrs(DialogFormTopDivP2Input, {
+    type: "checkbox",
+    id: "has-read",
+    name: "book_has-read",
+  });
+  DialogFormTopDivP2.appendChild(DialogFormTopDivP2Label);
+  DialogFormTopDivP2.appendChild(DialogFormTopDivP2Input);
+
+  DialogFormTopDiv.appendChild(DialogFormTopDivP1);
+  DialogFormTopDiv.appendChild(DialogFormTopDivP2);
+
+  // p3
+  const DialogFormTopP3 = document.createElement("p");
+  DialogFormTopP3.classList.add(...["dialog__form-top-p"]);
+  const DialogFormTopP3Label = document.createElement("label");
+  DialogFormTopP3Label.classList.add(
+    ...[
+      "dialog__form-label",
+      "dialog__form-label--image",
+      "_text",
+      "_text--normal",
+    ],
+  );
+  DialogFormTopP3Label.setAttribute("for", "image");
+  DialogFormTopP3Label.textContent = "Cover:";
+  const DialogFormTopP3Input = document.createElement("input");
+  DialogFormTopP3Input.classList.add(
+    ...[
+      "dialog__form-input--image",
+      "_text",
+      "_text--normal",
+      "_text--bold",
+      "--size-context-xs",
+    ],
+  );
+  this._setAttrs(DialogFormTopP3Input, {
+    type: "file",
+    id: "image",
+    name: "book_cover",
+    accept: "image/*.png, image/*.jpeg, image/*.jpg",
+  });
+
+  DialogFormTopP3.appendChild(DialogFormTopP3Label);
+  DialogFormTopP3.appendChild(DialogFormTopP3Input);
+  DialogFormTopP3.appendChild(document.createElement("span"));
+
+  dialogFormTop.appendChild(dialogFormPlease);
+  dialogFormTop.appendChild(DialogFormTopP1);
+  dialogFormTop.appendChild(DialogFormTopP2);
+  dialogFormTop.appendChild(DialogFormTopDiv);
+  dialogFormTop.appendChild(DialogFormTopP3);
+  dialogForm.appendChild(dialogFormTop);
+
+  // form bottom
+  const dialogFormBottom = document.createElement("div");
+  dialogFormBottom.classList.add("dialog__form-bottom");
+
+  const dialogFormCoverContainer = document.createElement("div");
+  dialogFormCoverContainer.classList.add("dialog__form-cover-container");
+  const dialogFormCoverImg = document.createElement("img");
+  dialogFormCoverImg.classList.add("dialog__form-cover-img");
+  this._setAttrs(dialogFormCoverImg, {
+    src: "",
+    alt: "BookCoverSelected",
+  });
+  dialogFormCoverContainer.appendChild(dialogFormCoverImg);
+  const dialogFormBottomDiv = document.createElement("div");
+  dialogFormBottomDiv.classList.add(
+    ...["dialog__form-bottom-div", "--size-context-rg"],
+  );
+  const dialogFormButton = document.createElement("div");
+  dialogFormButton.classList.add(
+    ...["dialog__form-button", "_text", "_text--header", "_text--bold"],
+  );
+  dialogFormButton.setAttribute("submit", "");
+  dialogFormButton.textContent = "Submit";
+  dialogFormBottomDiv.appendChild(dialogFormButton);
+
+  dialogFormBottom.appendChild(dialogFormCoverContainer);
+  dialogFormBottom.appendChild(dialogFormBottomDiv);
+  dialogForm.appendChild(dialogFormBottom);
+  // functionality
+
+  // -- image fallback if no image is specified
+  dialogFormCoverImg.onerror = () => {
+    dialogFormCoverImg.setAttribute("src", "./assets/images/default-cover.jpg");
   };
 
+  // adding everything together
+  dialog.appendChild(dialogHeader);
+  dialog.appendChild(dialogForm);
+};
+
+// display current books in library
+UILibraryHandler.prototype.buildExistingBooks = function () {
   for (let b of this.library.books) {
     // construct shell
     const cardFlexContainer = document.createElement("div");
@@ -126,14 +439,14 @@ UILibraryHandler.prototype.buildExistingBooks = function () {
       "svg",
     );
     cardBookmarkIcon.classList.add(...["card__bookmark-icon"]);
-    setAttrs(cardBookmarkIcon, {
+    this._setAttrs(cardBookmarkIcon, {
       viewBox: "0 0 24 24",
     });
     const cardBookmarkIconPath = document.createElementNS(
       "http://www.w3.org/2000/svg",
       "path",
     );
-    setAttrs(cardBookmarkIconPath, {
+    this._setAttrs(cardBookmarkIconPath, {
       d: "M17,3H7A2,2 0 0,0 5,5V21L12,18L19,21V5C19,3.89 18.1,3 17,3Z",
     });
     cardBookmarkIcon.appendChild(cardBookmarkIconPath);
@@ -164,7 +477,7 @@ UILibraryHandler.prototype.buildExistingBooks = function () {
       "svg",
     );
     cardButtonBookmarkSvg.classList.add(...["card__button-icon"]);
-    setAttrs(cardButtonBookmarkSvg, {
+    this._setAttrs(cardButtonBookmarkSvg, {
       height: "24px",
       viewBox: "0 -960 960 960",
       width: "24px",
@@ -173,7 +486,7 @@ UILibraryHandler.prototype.buildExistingBooks = function () {
       "http://www.w3.org/2000/svg",
       "path",
     );
-    setAttrs(cardButtonBookmarkPath, {
+    this._setAttrs(cardButtonBookmarkPath, {
       d: "M713-600 600-713l56-57 57 57 141-142 57 57-198 198ZM200-120v-640q0-33 23.5-56.5T280-840h240v80H280v518l200-86 200 86v-278h80v400L480-240 200-120Zm80-640h240-240Z",
     });
     cardButtonBookmarkSvg.appendChild(cardButtonBookmarkPath);
@@ -189,7 +502,7 @@ UILibraryHandler.prototype.buildExistingBooks = function () {
       "http://www.w3.org/2000/svg",
       "line",
     );
-    setAttrs(cardButtonDividerLine, {
+    this._setAttrs(cardButtonDividerLine, {
       x1: "50%",
       y1: "0",
       x2: "50%",
@@ -205,8 +518,8 @@ UILibraryHandler.prototype.buildExistingBooks = function () {
       "svg",
     );
     cardButtonDeleteSvg.classList.add(...["card__button-icon"]);
-    setAttrs(cardButtonDeleteSvg, { viewBox: "0 -960 960 960" });
-    setAttrs(cardButtonDeleteSvg, {
+    this._setAttrs(cardButtonDeleteSvg, { viewBox: "0 -960 960 960" });
+    this._setAttrs(cardButtonDeleteSvg, {
       height: "24px",
       width: "24px",
     });
@@ -214,7 +527,7 @@ UILibraryHandler.prototype.buildExistingBooks = function () {
       "http://www.w3.org/2000/svg",
       "path",
     );
-    setAttrs(cardButtonDeletePath, {
+    this._setAttrs(cardButtonDeletePath, {
       d: "M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z",
     });
     cardButtonDeleteSvg.appendChild(cardButtonDeletePath);
@@ -226,7 +539,7 @@ UILibraryHandler.prototype.buildExistingBooks = function () {
     cardBookImageContainer.classList.add(...["card__book-img-container"]);
     const cardBookImage = document.createElement("img");
     cardBookImage.classList.add(...["card__book-img"]);
-    setAttrs(cardBookImage, {
+    this._setAttrs(cardBookImage, {
       src: b.cover,
       alt: "Book Cover Image",
     });
@@ -319,20 +632,6 @@ UILibraryHandler.prototype.setSavedTheme = function () {
 
   if (localStorage.getItem(this.THEME_REF_NAME))
     this.root.setAttribute(this.THEME_REF_NAME, savedTheme);
-};
-
-// dialog creation helper
-UILibraryHandler.prototype.getDialog = function () {
-  const dialog = document.createElement("dialog");
-
-  dialog.classList.add(...["dialog", "--size-context-md"]);
-
-  // ensure dialog deletes itself after closing
-  dialog.addEventListener("close", () => {
-    dialog.remove();
-  });
-
-  return dialog;
 };
 
 function main() {
